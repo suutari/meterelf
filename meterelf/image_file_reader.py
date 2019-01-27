477619b37d72d7f1f3c9a2d5b179ed7be8d3654f
Tuomas Suutari <tuomas@nepnep.net>
2019-01-27 03:03:39 +0200

    image_file_reader: Add timing prints

--- meterelf/image_file_reader.py
+++ meterelf/image_file_reader.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 import time
+from contextlib import contextmanager
 from datetime import date, timedelta
 from glob import glob
 from itertools import groupby
@@ -56,6 +57,8 @@ class DataCollector:
             self,
             only_last_days: Optional[int] = None,
     ) -> None:
+        timer = Timer()
+        images_processed = 0
         start_date: date = (
             date.today() - timedelta(days=only_last_days)
             if only_last_days is not None else date(1900, 1, 1))
@@ -85,8 +88,54 @@ class DataCollector:
                     for path in sorted(glob(os.path.join(day_path, '*')))
                     if path.endswith(self.image_extensions)]
                 processor = _NewImageProcessorForDir(
-                    self.value_db, self.params_file, day_path)
+                    self.value_db, self.params_file, day_path, timer=timer)
                 process_in_blocks(images, processor.process_new_images)
+                images_processed += processor.processed_count
+        print()
+        print(f'Processed {images_processed} images')
+        print()
+        timer.print_timings(images_processed, 'image')
+
+
+class Timer:
+    def __init__(self) -> None:
+        self.start = time.time()
+        self.action_durations: Dict[str, float] = {}
+
+    @contextmanager
+    def time_action(self, name: str) -> Iterator[None]:
+        start = time.time()
+        try:
+            yield
+        finally:
+            end = time.time()
+            old_time = self.action_durations.get(name, 0.0)
+            self.action_durations[name] = old_time + (end - start)
+
+    def get_times(self) -> Dict[str, float]:
+        total = time.time() - self.start
+        time_on_other_actions = total - sum(self.action_durations.values())
+        return dict(
+            self.action_durations,
+            total=total,
+            other=time_on_other_actions)
+
+    def print_timings(self, units: int = 0, unit_name: str = 'unit') -> None:
+        times = self.get_times()
+        other = times.pop('other')
+        total = times.pop('total')
+        items = sorted(times.items())
+        items.append(('other activities', other))
+        items.append(('TOTAL', total))
+        print('Time spent on:')
+        for (name, duration) in items:
+            suffix = (
+                f', {1000 * duration / units :7.3f}ms / {unit_name}'
+                if units > 0 else '')
+            line = f'  {name:40} {duration:7.3f}s{suffix}'
+            if name == 'TOTAL':
+                print('=' * len(line))
+            print(line)
 
 
 class _NewImageProcessorForDir:
@@ -96,6 +145,7 @@ class _NewImageProcessorForDir:
             params_file: str,
             directory: str,
             do_replace: bool = False,
+            timer: Optional[Timer] = None,
     ) -> None:
         self.value_db = value_db
         self.params_file = params_file
@@ -103,9 +153,12 @@ class _NewImageProcessorForDir:
         self._day_dir = os.path.basename(directory)
         self._month_dir = os.path.basename(os.path.dirname(directory))
         self.do_replace = do_replace
+        self.timer: Timer = timer or Timer()
+        self.processed_count = 0
 
     def process_new_images(self, filenames: Sequence[str]) -> None:
-        paths = self._get_files_to_read(filenames)
+        with self.timer.time_action('finding new images'):
+            paths = list(self._get_files_to_read(filenames))
         self._read_data_and_enter_to_db(paths)
 
     def _get_files_to_read(self, filenames: Sequence[str]) -> Iterator[str]:
@@ -119,7 +172,8 @@ class _NewImageProcessorForDir:
                 yield os.path.join(self.directory, filename)
 
     def _read_data_and_enter_to_db(self, paths: Iterable[str]) -> None:
-        image_data = get_data_of_images(self.params_file, paths)
+        with self.timer.time_action('analyzing images'):
+            image_data = get_data_of_images(self.params_file, paths)
         entries = (
             Entry(
                 month_dir=self._month_dir,
@@ -129,14 +183,18 @@ class _NewImageProcessorForDir:
                 error=file_data[1],
                 modified_at=time.time())
             for (path, file_data) in image_data.items())
-        self.value_db.insert_entries(entries)
-        self.value_db.commit()
+        with self.timer.time_action('storing entries to database'):
+            self.value_db.insert_entries(entries)
+            self.value_db.commit()
+        self.processed_count += len(image_data)
 
 
 def get_data_of_images(
         params_file: str,
         paths: Iterable[str],
 ) -> Dict[str, Tuple[str, str]]:
+    if not paths:
+        return {}
     return dict(
         _format_image_data(data)
         for data in meterelf.get_meter_values(params_file, paths)
