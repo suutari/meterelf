bea6b319d8148ece819449863b376aed0c45d048
Tuomas Suutari <tuomas@nepnep.net>
2019-06-17 11:47:19 +0300

    process_data: Improve error correction

--- meterelf/store/process_data.py
+++ meterelf/store/process_data.py
@@ -1,7 +1,7 @@
 import sys
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import Callable, Iterator, List, Optional, Tuple
+from typing import Callable, Iterator, List, Optional, Sequence
 
 from ._fnparse import FilenameData
 from ._value_getter import ValueGetter, ValueRow
@@ -11,12 +11,82 @@ VALUE_MODULO = 1000
 VALUE_MAX_LEAP = 300  # litres (change per sample)
 VALUE_MAX_DIFFS = {
     'normal': 8.0,  # litres per second
+    'after_small_gap': 2.5,  # litres per second
     'reverse': 2.0,  # litres per second
+    'after_medium_gap': 1.5,  # litres per second
+    'after_big_gap': 1.0,  # litres per second
     'snapshot': 0.01,  # litres per second
 }
 MAX_CORRECTION = 0.05  # litres
 
-ValueRowPair = Tuple[ValueRow, Optional[ValueRow]]
+SMALL_GAP_DURATION = timedelta(seconds=3)
+MEDIUM_GAP_DURATION = timedelta(seconds=10)
+BIG_GAP_DURATION = timedelta(minutes=10)
+
+
+class ValueRowPointer:
+    def __init__(self, rows: Sequence[ValueRow], current_index: int) -> None:
+        assert len(rows) > current_index
+        self.rows = rows
+        self._current = current_index
+
+    @property
+    def current(self) -> ValueRow:
+        return self.rows[self._current]
+
+    def next(self, offset: int = 1) -> Optional[ValueRow]:
+        try:
+            return self.rows[self._current + offset]
+        except IndexError:
+            return None
+
+    def prev(self, offset: int = 1) -> Optional[ValueRow]:
+        return self.next(-offset)
+
+    def continuity_check(self, max_discard: int = 4) -> Optional[str]:
+        current = self.current
+        if current.reading is None:
+            return 'Unknown reading'
+        points = [
+            (x.reading, x.time) for x in self.rows
+            if x.reading is not None]
+        discard = min(len(points) - 3, max_discard)
+        if discard <= 1:
+            return None
+        has_smalls = any(x < 250 for (x, _) in points)
+        has_larges = any(x > 750 for (x, _) in points)
+        if has_smalls and has_larges:
+            points = [
+                (x + 1000, t) if x < 250 else (x, t)
+                for (x, t) in points]
+        points.sort()
+        middle = points[(discard // 2):-(discard // 2)]
+        if not middle:
+            return None
+        avg = sum(x for (x, _) in middle) / len(middle)
+        rng = max(x for (x, _) in middle) - min(x for (x, _) in middle)
+        diff_avg = abs(current.reading - avg)
+        if diff_avg > rng + 0.3:
+            return (
+                f'diff to avg = {diff_avg}, '
+                f'avg = {avg}, range = {rng}, '
+                f'got = {current.reading}')
+        first = min(middle, key=(lambda x: x[1]))
+        last = max(middle, key=(lambda x: x[1]))
+        litres = last[0] - first[0]
+        if litres <= 0.0:
+            return None
+        seconds = (last[1] - first[1]).total_seconds()
+        lps = litres / seconds
+        secs_since_first = (current.time - first[1]).total_seconds()
+        expected_reading = first[0] + secs_since_first * lps
+        diff = abs(current.reading - expected_reading)
+        if diff > 0.5:
+            return (
+                f'diff = {diff}, '
+                f'expected = {expected_reading}, '
+                f'got = {current.reading}')
+        return None
 
 
 @dataclass(frozen=True)
@@ -102,7 +172,9 @@ class DataProcessor:
         lfv = None  # Last Full Value
         ldt = None  # Last Date Time
 
-        for (row1, row2) in self.get_sorted_value_row_pairs():
+        for rowp in self.get_sorted_value_rows():
+            row1 = rowp.current
+            row2 = rowp.next()
             (dt, v, error, f, fn_data, modified_at) = row1
 
             def ignore(reason: str) -> InterpretedPoint:
@@ -119,6 +191,11 @@ class DataProcessor:
             # Sanity check
             if lv is not None and value_mod_diff(v, lv) > VALUE_MAX_LEAP:
                 yield ignore(f'Too big leap from {lv} to {v}')
+                continue
+
+            continuity_error = rowp.continuity_check()
+            if continuity_error:
+                yield ignore(f'Continuity check failed: {continuity_error}')
                 continue
 
             # Thousand counter
@@ -199,7 +276,10 @@ class DataProcessor:
                 next_value_goes_backward = (nfv is not None and nfv < fv)
                 diff_kind = (
                     'snapshot' if is_lonely_snapshot else
+                    'after_big_gap' if ddt > BIG_GAP_DURATION else
+                    'after_medium_gap' if ddt > MEDIUM_GAP_DURATION else
                     'reverse' if next_value_goes_backward else
+                    'after_small_gap' if ddt > SMALL_GAP_DURATION else
                     'normal')
                 if lps > VALUE_MAX_DIFFS[diff_kind]:
                     reason = f'big diff ({diff_kind})'
@@ -229,30 +309,29 @@ class DataProcessor:
             lv = v
             ldt = dt
 
-    def get_sorted_value_row_pairs(self) -> Iterator[ValueRowPair]:
-        result_buffer: List[ValueRow] = []
-
-        def push_to_buffer(item: ValueRow) -> None:
-            result_buffer.append(item)
-            result_buffer.sort()
-
-        def pop_from_buffer() -> ValueRowPair:
-            assert result_buffer
-            result: ValueRowPair
-            if len(result_buffer) >= 2:
-                result = (result_buffer[0], result_buffer[1])
-            else:
-                result = (result_buffer[0], None)
-            result_buffer.pop(0)
-            return result
-
+    def get_sorted_value_rows(
+            self,
+            window_size: int = 9,
+            minimum_size: int = 3,
+    ) -> Iterator[ValueRowPointer]:
+        window: List[ValueRow] = []
+        index: int = -minimum_size
         for entry in self.value_getter.get_values():
-            if len(result_buffer) >= 5:
-                yield pop_from_buffer()
-            push_to_buffer(entry)
+            window.append(entry)
+            window.sort()
+            index += 1
 
-        while result_buffer:
-            yield pop_from_buffer()
+            if len(window) >= minimum_size:
+                yield ValueRowPointer(window, index)
+
+            if len(window) >= window_size:
+                window.pop(0)
+                index -= 1
+
+        while window:
+            yield ValueRowPointer(window, max(0, index))
+            window.pop(0)
+            index -= 1
 
 
 def value_mod_diff(v1: float, v2: float) -> float:
